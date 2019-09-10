45c3140b3cd7140a94f523f624133899ba7d47ab
prudx <dnl.maguire@gmail.com>
2019-09-10 17:56:38 +0100

    Added todo list

--- rename.ps1
+++ rename.ps1
@@ -1,7 +1,13 @@
 ## -- Daniel Maguire --
 ## This is my first powershell script, it's purpose is to rename tv show episodes based on filetype and show name inputs
+##
+## Improvements to make
+## - Add support for movie renaming (name, (year) [quality]) through parameter 'type'
+## - Add support for other tv naming conventions like season01episode02
+## - Add file extension list so user doesn't need to specify filetype
 
 param (
+    [string]$type = "tv",
     [string]$extension = "mkv",
     [string]$name = $( Read-Host "Input show name please: " )
  )
